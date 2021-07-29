910f9204e06b4a768cdfe65e38ed0ae59f1144e6
vahid alvandi <va6893@gmail.com>
2021-07-29 17:06:46 +0430

    Update Verification.php
    a

--- src/Verification.php
+++ src/Verification.php
@@ -8,13 +8,15 @@ class Verification
 {
     /** @var int */
     private $amount;
+    private $merchant_id;
 
     /** @var string */
     private $authority;
 
-    public function __construct(int $amount)
+    public function __construct(int $amount, $merchant_id)
     {
         $this->amount = $amount;
+        $this->merchant_id = $merchant_id;
     }
 
     public function send(): VerificationResponse
@@ -24,7 +26,7 @@ class Verification
             : 'https://api.zarinpal.com/pg/v4/payment/verify.json';
 
         $data = [
-            'merchant_id' => config('zarinpal.merchant_id'),
+            'merchant_id' => $this->merchant_id,
             'amount' => $this->amount,
             'authority' => $this->authority,
         ];
